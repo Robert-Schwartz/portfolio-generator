526f9f74ab3e45f86c1fa44aad494aa1748327f8
Robert Schwartz <bschwartz23@gmail.com>
2021-05-05 21:55:46 -0700

    create module JS to generate webpage

--- app.js
+++ app.js
@@ -1,16 +1,9 @@
+const fs = require("fs");
+const generatePage = require("./src/page-template.js");
 const profileDataArgs = process.argv.slice(2, process.argv.length);
+const [name, github] = profileDataArgs;
 
-//function that takes in the array of command-line arguments
-const printProfileData = (profileDataArr) => {
-  // This...
-  for (let i = 0; i < profileDataArr.length; i += 1) {
-    console.log(profileDataArr[i]);
-  }
-
-  console.log("================");
-
-  // Is the same as this...
-  profileDataArr.forEach((profileItem) => console.log(profileItem));
-};
-
-printProfileData(profileDataArgs);
+fs.writeFile("./index.html", generatePage(name, github), (err) => {
+  if (err) throw new Error(err);
+  console.log("Portfolio complete! Check out index.html to see the output!");
+});
