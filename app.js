39ddc4d97e31e854ef05c5bdd77b4efbe7df6ed2
Robert Schwartz <bschwartz23@gmail.com>
2021-05-05 23:31:37 -0700

    9.3.5 add inquirer nppm

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
-//require file system built in node.js
-const fs = require("fs");
-//require the function from the src file
-const generatePage = require("./src/page-template.js");
-//capture arguments from command line array.  slice after 2
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-//can assign array indexes as const [0,1] post slice from profileDataArgs
-const [name, github] = profileDataArgs;
+const inquirer = require("inquirer");
+// const fs = require("fs");
+// const generatePage = require("./src/page-template");
 
-//write file using fs built in.  page name, content, error capture
-fs.writeFile("./index.html", generatePage(name, github), (err) => {
-  if (err) throw new Error(err);
-  console.log("Portfolio complete! Check out index.html to see the output!");
-});
+// const pageHTML = generatePage(name, github);
+
+// fs.writeFile("./index.html", pageHTML, (err) => {
+//   if (err) throw err;
+
+//   console.log("Portfolio complete! Check out index.html to see the output!");
+// });
+inquirer.prompt([
+  {
+    type: "input",
+    name: "name",
+    message: "What is your name?",
+  },
+])
+.then(answers => console.log(answers));
