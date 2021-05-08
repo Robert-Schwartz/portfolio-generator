df79b884001066a88d09a436257eb86ea1b9602d
Robert Schwartz <bschwartz23@gmail.com>
2021-05-07 19:06:25 -0700

    create Promise Chain refactoring

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
 const generatePage = require("./src/page-template");
+const { writeFile, copyFile } = require("./utils/generate-site.js");
 
 const promptUser = () => {
   return inquirer.prompt([
@@ -135,6 +136,47 @@ Add a New Project
     });
 };
 
+//----------prompt user callback using promise chain-------
+promptUser()
+  .then(promptProject)
+  .then((portfolioData) => {
+    return generatePage(portfolioData);
+  })
+  .then((pageHTML) => {
+    return writeFile(pageHTML);
+  })
+  .then((writeFileResponse) => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then((copyFileResponse) => {
+    console.log(copyFileResponse);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+//--------------explanation of promise chain:-------------------
+/*So let's reiterate the flow this function will now have:
+
+We start by asking the user for their information with Inquirer prompts; this returns all of the data as an object in a Promise.
+
+The promptProject() function captures the returning data from promptUser() and we recursively call promptProject() for as many projects as the user wants to add. Each project will be pushed into a projects array in the collection of portfolio information, and when we're done, the final set of data is returned to the next .then().
+
+The finished portfolio data object is returned as portfolioData and sent into the generatePage() function, which will return the finished HTML template code into pageHTML.
+
+We pass pageHTML into the newly created writeFile() function, which returns a Promise. This is why we use return here, so the Promise is returned into the next .then() method.
+
+Upon a successful file creation, we take the writeFileResponse object provided by the writeFile() function's resolve() execution to log it, and then we return copyFile().
+
+The Promise returned by copyFile() then lets us know if the CSS file was copied correctly, and if so, we're all done!
+
+This is where Promises really provide a benefit to the code readability. Most of what we had to run to make this project work was some form of asynchronous functionality, so actually executing it in a way that reads in a step-by-step manner helps make the code predictable.
+*/
+
+
+  //-----Prompt user using callbacks (old)-------
+/*  
 promptUser()
   .then(promptProject)
   .then((portfolioData) => {
@@ -156,3 +198,4 @@ promptUser()
       })
     });
   });
+  */
