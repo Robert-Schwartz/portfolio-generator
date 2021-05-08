2008fedf45f057300b252f5633ce9f5273fd73fe
Robert Schwartz <bschwartz23@gmail.com>
2021-05-07 17:01:42 -0700

    add Conditionally Create the About Section HTML

--- src/page-template.js
+++ src/page-template.js
@@ -1,14 +1,29 @@
+// create the about section
+const generateAbout = (aboutText) => {
+    if (!aboutText) {
+    return "";
+    }
+
+    return `
+    <section class="my-3" id="about">
+        <h2 class="text-dark bg-primary p-2 display-inline-block">About Me</h2>
+        <p>${aboutText}</p>
+    </section>
+    `;
+};
+
 module.exports = (templateData) => {
-  console.log(templateData);
+    console.log(templateData);
+
   // destructure projects and about data from templateData based on their property key names
   // this will create three variables based on data in templateData
-  const { projects, about, ...header } = templateData;
+    const { projects, about, ...header } = templateData;
 
-  return `
-  <!DOCTYPE html>
-  <html lang="en">
+    return `
+    <!DOCTYPE html>
+    <html lang="en">
 
-  <head>
+    <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
@@ -21,19 +36,23 @@ module.exports = (templateData) => {
     <body>
     <header>
         <div class="container flex-row justify-space-between align-center py-3">
-        <h1 class="page-title text-secondary bg-dark py-2 px-3">${header.name}</h1>
+        <h1 class="page-title text-secondary bg-dark py-2 px-3">${
+            header.name
+        }</h1>
         <nav class="flex-row">
-            <a class="ml-2 my-1 px-2 py-1 bg-secondary text-dark" href="https://github.com/${header.github}">GitHub</a>
+            <a class="ml-2 my-1 px-2 py-1 bg-secondary text-dark" href="https://github.com/${
+                header.github
+            }">GitHub</a>
         </nav>
         </div>
     </header>
     <main class="container my-5">
-
+            ${generateAbout(about)}
     </main>
     <footer class="container text-center py-3">
         <h3 class="text-dark">&copy; ${new Date().getFullYear()} by ${
     header.name
-  }</h3>
+    }</h3>
     </footer>
     </body>
     </html>
